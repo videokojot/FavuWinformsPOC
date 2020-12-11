96bb1497e9eddfa0936d4f28470a633e63c7c1ec
Jindrich Cincibuch <jindrich.cincibuch@gmail.com>
2020-12-11 23:00:25 +0100

    Added action

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -70,6 +70,7 @@ namespace WindowsFormsApp1
             this.button2.TabIndex = 1;
             this.button2.Text = "Frisk";
             this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
--- Form1.cs
+++ Form1.cs
@@ -21,5 +21,10 @@ namespace WindowsFormsApp1
         {
 
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("You have frisked!");
+        }
     }
 }
